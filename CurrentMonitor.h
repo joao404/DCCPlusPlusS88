6a6d34f0e898e89f5987a5e9fffc31e9e61f4689
joao404 <mmaage92@googlemail.com>
2019-04-17 12:41:51 +0200

    Basisstand mit S88

---
+++ CurrentMonitor.h
@@ -0,0 +1,36 @@
+/**********************************************************************
+
+CurrentMonitor.h
+COPYRIGHT (c) 2013-2015 Gregg E. Berman
+
+Part of DCC++ BASE STATION for the Arduino
+
+**********************************************************************/
+
+#ifndef CurrentMonitor_h
+#define CurrentMonitor_h
+
+#include "Arduino.h"
+
+#define  CURRENT_SAMPLE_SMOOTHING   0.01
+#define  CURRENT_SAMPLE_MAX         190       //300
+
+#ifdef ARDUINO_AVR_UNO                        // Configuration for UNO
+  #define  CURRENT_SAMPLE_TIME        10
+#else                                         // Configuration for MEGA    
+  #define  CURRENT_SAMPLE_TIME        1
+#endif
+
+struct CurrentMonitor{
+  static long int sampleTime;
+  int pin;
+  float current;
+  int offset;
+  char *msg;
+  CurrentMonitor(int, char *);
+  static boolean checkTime();
+  void check();
+};
+
+#endif
+
